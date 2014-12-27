75a94c056a68f2990e0d356a9cd0e7c9f74c043e
tetang1230 <tetang1230@gmail.com>
2014-12-27 10:18:18 +0800

    微信自动回复和分享功能总结

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ weixin
 微信分享,公众号开发
 
 ```conf
+
+_微信自动回复功能_
+
 1. 登陆https://mp.weixin.qq.com/
 2. 点击“开发者中心”
 3. 设置微信调用url,token
+4. 以yaf框架为例,wechat.php中的Event201408 action去接受微信服务器的调用,这个action的url你要配置到微信公众后台
+5. thinkwechat是接受weixin请求后,验签和解析xml的操作
+
+
+_微信分享功能_
+1. 下载这个项目,已经说明的非常详细了,链接：https://github.com/zxlie/WeixinApi
+
 ```
---
+++ Wechat.php
@@ -0,0 +1,67 @@
+<?php
+class WeChatController extends Yaf_Controller_Abstract
+{
+
+    public function Event201408Action()
+    {
+        $token  = '你在weixin公众号管理后台设置的token,copy到这';
+        $weixin = new ThinkWechat($token);
+
+        /* 获取请求信息 */
+        $req_data = $weixin->request();
+        $keywords = array('keyword1', 'keyword2');
+
+        do
+        {
+            // 新用户关注
+            if('event' == $req_data['MsgType'] && 'subscribe' == $req_data['Event'])
+            {
+                $content  = '感谢关注XXOO，精彩内容与大家分享～～' . "\n";
+                $content .= '发送“keyword1”或“keyword2”即刻参与活动！';
+
+                break;
+            }
+
+            // 处理用户发送的内容
+            $break_flag = 1;
+            $content    = '输入有误,请输入: "' . implode('"或"', $keywords) . '"';
+            foreach ($keywords as $k)
+            {
+                if ($k == trim($req_data['Content']))
+                {
+                    $break_flag = 0;
+                    break;
+                }
+            }
+            if ($break_flag)
+            {
+                // 不给返回错误提示
+                exit('no wrong notice');
+                //break;
+            }
+
+            // 下放激活码
+            // 以下是你自己的相关业务逻辑,例如发码
+            $event_data = AppEvent201408Model::getEventCode($req_data);
+            if (false === $event_data)
+            {
+                //$content = '出错了,请稍候再试.';
+                exit('has wrong');
+            }
+            elseif (0 == count($event_data))
+            {
+                //$content = '对不起,您来晚了,激活码全部送完.';
+                exit('over');
+            }
+            else
+            {
+                $content  = "您的激活码是：（{$event_data['event_code']}），请尽快兑换。";
+                $content .= '<a href="http://xxoo.com/2014/0826/index.php">点我立刻兑换</a>';
+            }
+        } while(0);
+
+        $weixin->response($content);
+    }
+
+}
+
